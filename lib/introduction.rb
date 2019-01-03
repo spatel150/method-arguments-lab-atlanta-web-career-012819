afa0bbfd42a81935f7664a5792c4745fdfa67a4b
Samidh Patel <spatel13456@gmail.com>
2019-01-03 21:29:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,4 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
-introduction("Josh")
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
